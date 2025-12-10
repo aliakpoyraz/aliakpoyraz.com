e862949bfe50ec7deecfa20fc89d7641f08fec10
Ali Akpoyraz <128495051+aliakpoyraz@users.noreply.github.com>
2025-12-10 12:06:01 +0300

    aliakpoyraz.com : cloudflare pages update's

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  eslint: {
+    // ESLint'i build sırasında devre dışı bırak
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   async redirects() {
     return [];
   },
